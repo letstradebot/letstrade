db930571a497b69fa5f69bbb2f8fc2fb9e642c4c
clark1816 <clark.mcquiston@gmail.com>
2022-10-14 19:20:46 -0400

    updates
    updated supervisor.d

--- tradingbot.py
+++ tradingbot.py
@@ -34,95 +34,97 @@ for row in rows:
 
 
 def trade():
-    #get the current date 
-    my_date = date.today()
-    #when using the 200 sma it is nice to look further back
-    last_week = my_date - timedelta(days = 7)
-    diff = max(1, (my_date.weekday() + 6) % 7 - 3)
-    #calculate yesterda as business day
-    yes_bid = my_date - timedelta(days=diff)
+    if rows:
+        #get the current date 
+        my_date = date.today()
+        #when using the 200 sma it is nice to look further back
+        last_week = my_date - timedelta(days = 7)
+        diff = max(1, (my_date.weekday() + 6) % 7 - 3)
+        #calculate yesterda as business day
+        yes_bid = my_date - timedelta(days=diff)
 
-    tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
-    BASE_URL = config.URL_ENDPT
-    x = 1
-    #get active postions 
-    x = 1
-    tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
-    orders = tradeing_client.list_positions()  
-    order_symbols = [order.symbol for order in orders if x == 1]
+        tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
+        BASE_URL = config.URL_ENDPT
+        x = 1
+        #get active postions 
+        x = 1
+        tradeing_client = api.REST(config.API_Key, config.API_Secret, config.URL_ENDPT)
+        orders = tradeing_client.list_positions()  
+        order_symbols = [order.symbol for order in orders if x == 1]
 
-    for symbol in symbols:
-        if symbol not in order_symbols:
-            print(f"Symbol {symbol} does not have an active trade will look for trade to enter")
-            
-            #get price data from alpaca api
-            bars = yf.download(tickers = symbol,start=last_week, end=my_date, interval = '5m', rounding = True )
-            print(my_date)
-            bars.head()
-            # #put bars into np array
-            price = [bar for bar in bars['Close'] if bar != "NaN"]
-            prices = np.array(price)
-            open_bar = [bar for bar in bars['Open'] if bar != "NaN"]
-            opens = np.array(open_bar)
-            high_bar = [bar for bar in bars['High'] if bar != "NaN"]
-            highs = np.array(high_bar)
-            low_bar = [bar for bar in bars['Low'] if bar != "NaN"]
-            lows = np.array(low_bar)
-            volume_bar = [bar for bar in bars['Volume'] if bar != "NaN"]
-            volumes = np.array(volume_bar)
-            #define the TA-LIB tools that I want to use to base my strat around 
-            print("go here")
-            if len(prices) > 51:
-                RSI = talib.RSI(prices,14)
-                SMA_200 = talib.SMA(prices, timeperiod=200)
-                SMA_50 = talib.SMA(prices, timeperiod=50)
-            engulfing = talib.CDLENGULFING(opens, highs, lows, prices)
-            
-            # #Get the value of the most recent element in the np array.
-            length_sma_200 = (len(SMA_200))-1
-            current_SMA_200 = SMA_200[length_sma_200]
-            length_sma_50 = (len(SMA_50))-1
-            current_SMA_50 = SMA_50[length_sma_50]
-            length_RSI = (len(RSI))-1
-            current_RSI = RSI[length_RSI]
-            len_engulfing = (len(engulfing))-1
-            current_engulfing = engulfing[len_engulfing]
-            length_price = (len(prices))-1
-            current_price = prices[length_price]
-            #define params for taking profit and stoping
-            take_profit = round((current_price*.05)+current_price,2)
-            lost_profit = round(current_price-(current_price*.025),2)
-            #now that we have all the elements we need we can create the parameteres for the strat.
-            print(f"current stock {symbol}")
-            print(f"current Price: {current_price}")
-            print(f"current SMA 200: {current_SMA_200}")
-            print(f"current SMA 50: {current_SMA_50}")
-            print(f"current RSI 14: {current_RSI}")
-            print(f"Candle patter: {current_engulfing}")
+        for symbol in symbols:
+            if symbol not in order_symbols:
+                print(f"Symbol {symbol} does not have an active trade will look for trade to enter")
+                
+                #get price data from alpaca api
+                bars = yf.download(tickers = symbol,start=last_week, end=my_date, interval = '5m', rounding = True )
+                print(my_date)
+                bars.head()
+                # #put bars into np array
+                price = [bar for bar in bars['Close'] if bar != "NaN"]
+                prices = np.array(price)
+                open_bar = [bar for bar in bars['Open'] if bar != "NaN"]
+                opens = np.array(open_bar)
+                high_bar = [bar for bar in bars['High'] if bar != "NaN"]
+                highs = np.array(high_bar)
+                low_bar = [bar for bar in bars['Low'] if bar != "NaN"]
+                lows = np.array(low_bar)
+                volume_bar = [bar for bar in bars['Volume'] if bar != "NaN"]
+                volumes = np.array(volume_bar)
+                #define the TA-LIB tools that I want to use to base my strat around 
+                print("go here")
+                if len(prices) > 51:
+                    RSI = talib.RSI(prices,14)
+                    SMA_200 = talib.SMA(prices, timeperiod=200)
+                    SMA_50 = talib.SMA(prices, timeperiod=50)
+                engulfing = talib.CDLENGULFING(opens, highs, lows, prices)
+                
+                # #Get the value of the most recent element in the np array.
+                length_sma_200 = (len(SMA_200))-1
+                current_SMA_200 = SMA_200[length_sma_200]
+                length_sma_50 = (len(SMA_50))-1
+                current_SMA_50 = SMA_50[length_sma_50]
+                length_RSI = (len(RSI))-1
+                current_RSI = RSI[length_RSI]
+                len_engulfing = (len(engulfing))-1
+                current_engulfing = engulfing[len_engulfing]
+                length_price = (len(prices))-1
+                current_price = prices[length_price]
+                #define params for taking profit and stoping
+                take_profit = round((current_price*.05)+current_price,2)
+                lost_profit = round(current_price-(current_price*.025),2)
+                #now that we have all the elements we need we can create the parameteres for the strat.
+                print(f"current stock {symbol}")
+                print(f"current Price: {current_price}")
+                print(f"current SMA 200: {current_SMA_200}")
+                print(f"current SMA 50: {current_SMA_50}")
+                print(f"current RSI 14: {current_RSI}")
+                print(f"Candle patter: {current_engulfing}")
 
 
-            #time to write the code that will conduct autotrading for me
-            if current_price > current_SMA_50 and current_price > current_SMA_200 and current_engulfing > 0:
-                    print(f"Entering a Trade with stock {symbol} at price {current_price}")
-                    tradeing_client.submit_order(
-                        symbol=symbol,
-                        side='buy',
-                        type='market',
-                        qty='1',
-                        time_in_force='day',
-                        order_class='bracket',
-                        take_profit=dict(
-                            limit_price=take_profit,
-                        ),
-                        stop_loss=dict(
-                            stop_price=lost_profit,
-                            limit_price=lost_profit,
-                        )
-                    )   
-
+                #time to write the code that will conduct autotrading for me
+                if current_price > current_SMA_50 and current_price > current_SMA_200 and current_engulfing > 0:
+                        print(f"Entering a Trade with stock {symbol} at price {current_price}")
+                        tradeing_client.submit_order(
+                            symbol=symbol,
+                            side='buy',
+                            type='market',
+                            qty='1',
+                            time_in_force='day',
+                            order_class='bracket',
+                            take_profit=dict(
+                                limit_price=take_profit,
+                            ),
+                            stop_loss=dict(
+                                stop_price=lost_profit,
+                                limit_price=lost_profit,
+                            )
+                        )   
+    else:
+        print("no stocks in watchlist")
                     
-        
-    print("Code Completed")
+    
+print("Code Completed")
 sc.every(10).seconds.do(trade)
 
 #do program stuff to figure out if the stock market is open
@@ -141,10 +143,15 @@ while exchange.size > 0:
     print(m3)
     
     if m3 < '16:30:00' and m3 > '09:30:00':
-        #print(current_time)
+        print(current_time)
         sc.run_pending()
-        time.sleep(290)    
+        time.sleep(290) 
+
     else:
         print("market currently not open")
         time.sleep(30) 
+        
 
+
+
+print("code completed")
